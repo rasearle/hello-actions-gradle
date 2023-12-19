992d4b2823a49cfad9bff7164d59d32d5b045b1d
Searle <richard.searle@tsb.co.uk>
2023-12-19 16:43:35 +0000

    add untested code

--- app/src/main/java/com/example/App.java
+++ app/src/main/java/com/example/App.java
@@ -8,6 +8,10 @@ public class App {
         return "Hello World!";
     }
 
+    String testMe() {
+        return "I need to be tested";
+    }
+
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
     }
