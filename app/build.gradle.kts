8f0ec8777e039be18cb92ddd078d1beb1ca628ab
Searle <richard.searle@tsb.co.uk>
2023-12-19 09:21:57 +0000

    add jacoco plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,7 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+    id("jacoco")
     id("org.sonarqube") version "4.4.1.3373"
 }
 
